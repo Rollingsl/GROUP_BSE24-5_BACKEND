7a9485641d73cea9c2717c72abe1301eeff56882
cynthia2000boop <musiimentacynthia01@gmail.com>
2024-10-15 01:35:25 -0700

    added1

--- src/controllers/rateLimitController.ts
+++ src/controllers/rateLimitController.ts
@@ -2,11 +2,11 @@ import { rateLimit } from "express-rate-limit";
 
 let requestLimit: number;
 let requestWindowMs: number;
-
+// Variables to store request limit and time window for rate limiting
 const isDevEnvironment: boolean = process.env.NODE_ENV === "developments";
 const isProdEnvironment: boolean = process.env.NODE_ENV === "production";
 const isTestEnvironment: boolean = process.env.NODE_ENV === "test";
-
+// Check the current environment based on the NODE_ENV environment variable
 if (isDevEnvironment || isProdEnvironment) {
   requestLimit = 75;
   requestWindowMs = 60 * 1000 * 5; // 5 min
